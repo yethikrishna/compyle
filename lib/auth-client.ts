52649914093e937ed8c5592a267cd52b363feb3f
tracepanic <patrickscript@proton.me>
2025-11-10 08:49:20 +0300

    Add User Nav and Dropdown Menu Components

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 
-export const { signUp, signIn, useSession, sendVerificationEmail } =
+export const { signUp, signIn, useSession, sendVerificationEmail, signOut } =
   createAuthClient({
     baseURL: process.env.BETTER_AUTH_URL!,
   });
