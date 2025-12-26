8f998f258964142e27d814cd7249d1776a3f13da
tracepanic <patrickscript@proton.me>
2025-12-26 13:14:37 +0300

    Fix SEO issues in terms of missing images

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -18,13 +18,13 @@ export async function generateMetadata({
       openGraph: {
         title: `${data.name} | Compyle Apps`,
         description: data.about,
-        images: [{ url: data.image || "" }],
+        ...(data.image && { images: [{ url: data.image }] }),
       },
       twitter: {
         card: "summary_large_image",
         title: `${data.name} | Compyle Apps`,
         description: data.about,
-        images: [{ url: data.image || "" }],
+        ...(data.image && { images: [{ url: data.image }] }),
       },
     };
   } catch {
